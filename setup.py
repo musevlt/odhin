c1b5b54d14a9aa460a75c04ed379f0d7b8e86996
rbacher <bacher.raphael@gmail.com>
2017-02-22 15:26:00 +0100

    add scikit-image dependence

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='deblend',                                                              
     version='0.1',                                                             
     install_requires=['numpy', 'matplotlib', 'astropy', 
-                       'scipy','adjustText','termcolor'],
+                       'scipy','adjustText','termcolor','scikit-image'],
     packages=find_packages(),                                                  
     zip_safe=False,                                                            
     package_data={                                                             
