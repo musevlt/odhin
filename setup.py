958f2201cdc1896238c87c6b6cfede2b9500f553
rbacher <bacher.raphael@gmail.com>
2017-02-22 10:00:30 +0100

    update dependencies

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(                                                                         
     name='deblend',                                                              
     version='0.1',                                                             
+    install_requires=['numpy', 'matplotlib', 'astropy', 'mpdaf',
+                       'scipy','adjustText','termcolor'],
     packages=find_packages(),                                                  
     zip_safe=False,                                                            
     package_data={                                                             
