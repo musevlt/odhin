b60100059c4256dac76ff64c775376e4e0ac8756
rbacher <bacher.raphael@gmail.com>
2017-06-28 16:55:29 +0200

    add mpdaf to dependancies

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='deblend',
     version='0.2',
-    install_requires=['scikit-image','sklearn','scipy','numpy', 'matplotlib', 'astropy', 
+    install_requires=['scikit-image','sklearn','mpdaf','scipy','numpy', 'matplotlib', 'astropy', 
                        'adjustText','termcolor'],
     packages=find_packages(),
     zip_safe=False,
