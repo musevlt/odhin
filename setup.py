923c3bb3c1c41973c4b881ab04d21fb26ba59891
rbacher <bacher.raphael@gmail.com>
2017-06-28 16:53:10 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from setuptools import setup, find_packages                                    
-                                                                               
-setup(                                                                         
-    name='deblend',                                                              
-    version='0.1',                                                             
-    install_requires=['numpy', 'matplotlib', 'astropy', 
-                       'scipy','adjustText','termcolor','scikit-image','sklearn'],
-    packages=find_packages(),                                                  
-    zip_safe=False,                                                            
-    package_data={                                                             
+from setuptools import setup, find_packages
+
+setup(
+    name='deblend',
+    version='0.2',
+    install_requires=['scikit-image','sklearn','scipy','numpy', 'matplotlib', 'astropy', 
+                       'adjustText','termcolor'],
+    packages=find_packages(),
+    zip_safe=False,
+    package_data={
         'deblend': ['deblend/*.dat'],                                               
-    },                                                                         
-    include_package_data=True,                                                 
+    },
+    include_package_data=True,
 ) 
+
