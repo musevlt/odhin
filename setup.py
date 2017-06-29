e5c411a877cfcf6655ae4d5dfd5a0cde7fb59756
rbacher <bacher.raphael@gmail.com>
2017-06-29 14:27:49 +0200

    remove mpdaf dependence (to be install on its own)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='deblend',
     version='0.2',
-    install_requires=['scikit-learn','mpdaf','scipy','numpy', 'matplotlib', 'astropy', 
+    install_requires=['scikit-learn','scipy','numpy', 'matplotlib', 'astropy', 
                        'adjustText','termcolor'],
     packages=find_packages(),
     zip_safe=False,
