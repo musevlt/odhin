76d0f75943e7b9f078a495253f240fdf60cce07e
Raphael Bacher <raphael.bacher@univ-grenoble-alpes.fr>
2018-07-12 07:09:50 +0200

    pep8

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='deblend',
     version='0.2',
-    install_requires=['scikit-learn','scipy','numpy', 'matplotlib', 'astropy', 'mpdaf',
-                       'adjustText','termcolor','photutils'],
+    install_requires=['scikit-learn', 'scipy', 'numpy', 'matplotlib', 'astropy', 'mpdaf',
+                      'adjustText', 'termcolor', 'photutils'],
     packages=find_packages(),
     zip_safe=False,
     package_data={
-        'deblend': ['data/*.dat'],                                               
+        'deblend': ['data/*.dat'],
     },
     include_package_data=True,
-) 
-
+)
