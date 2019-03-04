e99e36fb420b9c5ea6627ad7c01effc1a3ef34ee
Simon Conseil <contact@saimon.org>
2019-03-04 14:51:52 +0100

    Remove adjustText and termcolor from dependencies

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='odhin',
     version='1.0-beta2',
-    install_requires=['scikit-learn', 'scipy', 'numpy', 'matplotlib', 'astropy', 'mpdaf',
-                      'adjustText', 'termcolor', 'photutils','tqdm'],
+    install_requires=['scikit-learn', 'scipy', 'numpy', 'matplotlib',
+                      'astropy', 'mpdaf', 'photutils', 'tqdm'],
     packages=find_packages(),
     zip_safe=False,
     package_data={
